d32000b8b94c77a47a602e07b896ffb3354e1337
Osama.Marhabi <osama.almarhabi@ultimate-eg.net>
2026-01-24 00:14:14 +0300

    add minu

--- Dari/Form1.Designer.cs
+++ Dari/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Dari
+namespace Dari
 {
     partial class Form1
     {
@@ -29,11 +29,187 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pnlSidebar = new System.Windows.Forms.Panel();
+            this.btnDataManagement = new MaterialSkin.Controls.MaterialButton();
+            this.btnOperations = new MaterialSkin.Controls.MaterialButton();
+            this.btnReports = new MaterialSkin.Controls.MaterialButton();
+            this.btnSettings = new MaterialSkin.Controls.MaterialButton();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.lblHeaderTitle = new MaterialSkin.Controls.MaterialLabel();
+            this.btnMenuToggle = new MaterialSkin.Controls.MaterialButton();
+            this.pnlSidebar.SuspendLayout();
+            this.pnlContent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSidebar
+            // 
+            this.pnlSidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(150)))), ((int)(((byte)(243)))));
+            this.pnlSidebar.Controls.Add(this.btnMenuToggle);
+            this.pnlSidebar.Controls.Add(this.btnSettings);
+            this.pnlSidebar.Controls.Add(this.btnReports);
+            this.pnlSidebar.Controls.Add(this.btnOperations);
+            this.pnlSidebar.Controls.Add(this.btnDataManagement);
+            this.pnlSidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSidebar.Location = new System.Drawing.Point(0, 0);
+            this.pnlSidebar.Name = "pnlSidebar";
+            this.pnlSidebar.Size = new System.Drawing.Size(250, 700);
+            this.pnlSidebar.TabIndex = 0;
+            // 
+            // btnMenuToggle
+            // 
+            this.btnMenuToggle.AutoSize = false;
+            this.btnMenuToggle.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnMenuToggle.Depth = 0;
+            this.btnMenuToggle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMenuToggle.DrawShadows = false;
+            this.btnMenuToggle.HighEmphasis = false;
+            this.btnMenuToggle.Icon = null;
+            this.btnMenuToggle.Location = new System.Drawing.Point(0, 0);
+            this.btnMenuToggle.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnMenuToggle.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnMenuToggle.Name = "btnMenuToggle";
+            this.btnMenuToggle.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnMenuToggle.Size = new System.Drawing.Size(250, 50);
+            this.btnMenuToggle.TabIndex = 0;
+            this.btnMenuToggle.Text = "☰";
+            this.btnMenuToggle.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Text;
+            this.btnMenuToggle.UseAccentColor = false;
+            this.btnMenuToggle.UseVisualStyleBackColor = true;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.AutoSize = false;
+            this.btnSettings.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSettings.Depth = 0;
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSettings.DrawShadows = false;
+            this.btnSettings.HighEmphasis = false;
+            this.btnSettings.Icon = null;
+            this.btnSettings.Location = new System.Drawing.Point(0, 50);
+            this.btnSettings.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnSettings.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnSettings.Size = new System.Drawing.Size(250, 50);
+            this.btnSettings.TabIndex = 1;
+            this.btnSettings.Text = "⚙️ الإعدادات";
+            this.btnSettings.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Text;
+            this.btnSettings.UseAccentColor = false;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // btnReports
+            // 
+            this.btnReports.AutoSize = false;
+            this.btnReports.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnReports.Depth = 0;
+            this.btnReports.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReports.DrawShadows = false;
+            this.btnReports.HighEmphasis = false;
+            this.btnReports.Icon = null;
+            this.btnReports.Location = new System.Drawing.Point(0, 100);
+            this.btnReports.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnReports.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnReports.Name = "btnReports";
+            this.btnReports.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnReports.Size = new System.Drawing.Size(250, 50);
+            this.btnReports.TabIndex = 2;
+            this.btnReports.Text = "📈 التقارير";
+            this.btnReports.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Text;
+            this.btnReports.UseAccentColor = false;
+            this.btnReports.UseVisualStyleBackColor = true;
+            // 
+            // btnOperations
+            // 
+            this.btnOperations.AutoSize = false;
+            this.btnOperations.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnOperations.Depth = 0;
+            this.btnOperations.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOperations.DrawShadows = false;
+            this.btnOperations.HighEmphasis = false;
+            this.btnOperations.Icon = null;
+            this.btnOperations.Location = new System.Drawing.Point(0, 150);
+            this.btnOperations.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnOperations.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnOperations.Name = "btnOperations";
+            this.btnOperations.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnOperations.Size = new System.Drawing.Size(250, 50);
+            this.btnOperations.TabIndex = 3;
+            this.btnOperations.Text = "💼 العمليات";
+            this.btnOperations.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Text;
+            this.btnOperations.UseAccentColor = false;
+            this.btnOperations.UseVisualStyleBackColor = true;
+            // 
+            // btnDataManagement
+            // 
+            this.btnDataManagement.AutoSize = false;
+            this.btnDataManagement.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnDataManagement.Depth = 0;
+            this.btnDataManagement.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDataManagement.DrawShadows = false;
+            this.btnDataManagement.HighEmphasis = false;
+            this.btnDataManagement.Icon = null;
+            this.btnDataManagement.Location = new System.Drawing.Point(0, 200);
+            this.btnDataManagement.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnDataManagement.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnDataManagement.Name = "btnDataManagement";
+            this.btnDataManagement.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnDataManagement.Size = new System.Drawing.Size(250, 50);
+            this.btnDataManagement.TabIndex = 4;
+            this.btnDataManagement.Text = "📊 البيانات الأساسية";
+            this.btnDataManagement.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Text;
+            this.btnDataManagement.UseAccentColor = false;
+            this.btnDataManagement.UseVisualStyleBackColor = true;
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.BackColor = System.Drawing.Color.White;
+            this.pnlContent.Controls.Add(this.lblHeaderTitle);
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(250, 0);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Padding = new System.Windows.Forms.Padding(20);
+            this.pnlContent.Size = new System.Drawing.Size(950, 700);
+            this.pnlContent.TabIndex = 1;
+            // 
+            // lblHeaderTitle
+            // 
+            this.lblHeaderTitle.AutoSize = true;
+            this.lblHeaderTitle.Depth = 0;
+            this.lblHeaderTitle.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblHeaderTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblHeaderTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblHeaderTitle.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblHeaderTitle.Name = "lblHeaderTitle";
+            this.lblHeaderTitle.Size = new System.Drawing.Size(200, 19);
+            this.lblHeaderTitle.TabIndex = 0;
+            this.lblHeaderTitle.Text = "مرحباً بك في نظام إدارة الإيجارات";
+            // 
+            // Form1
+            // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.pnlSidebar);
+            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "نظام إدارة إيجارات المباني";
+            this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+            this.pnlSidebar.ResumeLayout(false);
+            this.pnlContent.ResumeLayout(false);
+            this.pnlContent.PerformLayout();
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlSidebar;
+        private MaterialSkin.Controls.MaterialButton btnMenuToggle;
+        private MaterialSkin.Controls.MaterialButton btnSettings;
+        private MaterialSkin.Controls.MaterialButton btnReports;
+        private MaterialSkin.Controls.MaterialButton btnOperations;
+        private MaterialSkin.Controls.MaterialButton btnDataManagement;
+        private System.Windows.Forms.Panel pnlContent;
+        private MaterialSkin.Controls.MaterialLabel lblHeaderTitle;
     }
 }
-
--- Dari/Form1.cs
+++ Dari/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace Dari
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
+        private bool isMenuCollapsed = false;
+        private const int MENU_EXPANDED_WIDTH = 250;
+        private const int MENU_COLLAPSED_WIDTH = 60;
+
         public Form1()
         {
             InitializeComponent();
+            
+            // إعداد MaterialSkin مع دعم RTL للعربية
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue700, Primary.Blue800,
+                Primary.Blue500, Accent.Blue200,
+                TextShade.WHITE
+            );
+            
+            // تفعيل دعم RTL للعربية
+            this.RightToLeft = RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            
+            // إعداد القائمة الجانبية
+            SetupSidebarMenu();
+            
+            // ربط الأحداث
+            btnMenuToggle.Click += BtnMenuToggle_Click;
+            btnDataManagement.Click += BtnDataManagement_Click;
+            btnOperations.Click += BtnOperations_Click;
+            btnReports.Click += BtnReports_Click;
+            btnSettings.Click += BtnSettings_Click;
+        }
+
+        private void SetupSidebarMenu()
+        {
+            // القائمة مفتوحة افتراضياً
+            isMenuCollapsed = false;
+            pnlSidebar.Width = MENU_EXPANDED_WIDTH;
+            
+            // إعداد الأزرار
+            UpdateMenuButtons();
+        }
+
+        private void BtnMenuToggle_Click(object sender, EventArgs e)
+        {
+            ToggleMenu();
+        }
+
+        private void ToggleMenu()
+        {
+            isMenuCollapsed = !isMenuCollapsed;
+            
+            // تحريك القائمة مع تأثير سلس
+            if (isMenuCollapsed)
+            {
+                // طي القائمة - عرض الأيقونات فقط
+                pnlSidebar.Width = MENU_COLLAPSED_WIDTH;
+            }
+            else
+            {
+                // فتح القائمة - عرض النصوص الكاملة
+                pnlSidebar.Width = MENU_EXPANDED_WIDTH;
+            }
+            
+            UpdateMenuButtons();
+        }
+
+        private void UpdateMenuButtons()
+        {
+            // زر القائمة يبقى أيقونة فقط
+            btnMenuToggle.Text = "☰";
+            
+            if (isMenuCollapsed)
+            {
+                // عند الطي: إظهار الأيقونات فقط
+                btnSettings.Text = "⚙️";
+                btnReports.Text = "📈";
+                btnOperations.Text = "💼";
+                btnDataManagement.Text = "📊";
+            }
+            else
+            {
+                // عند الفتح: إظهار النصوص الكاملة
+                btnSettings.Text = "⚙️ الإعدادات";
+                btnReports.Text = "📈 التقارير";
+                btnOperations.Text = "💼 العمليات";
+                btnDataManagement.Text = "📊 البيانات الأساسية";
+            }
+        }
+
+        private void BtnDataManagement_Click(object sender, EventArgs e)
+        {
+            lblHeaderTitle.Text = "البيانات الأساسية";
+            // هنا سيتم فتح شاشة البيانات الأساسية لاحقاً
+        }
+
+        private void BtnOperations_Click(object sender, EventArgs e)
+        {
+            lblHeaderTitle.Text = "العمليات";
+            // هنا سيتم فتح شاشة العمليات لاحقاً
+        }
+
+        private void BtnReports_Click(object sender, EventArgs e)
+        {
+            lblHeaderTitle.Text = "التقارير";
+            // هنا سيتم فتح شاشة التقارير لاحقاً
+        }
+
+        private void BtnSettings_Click(object sender, EventArgs e)
+        {
+            lblHeaderTitle.Text = "الإعدادات";
+            // هنا سيتم فتح شاشة الإعدادات لاحقاً
         }
     }
 }
